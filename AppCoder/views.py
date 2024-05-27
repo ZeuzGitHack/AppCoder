9fd5dd5164d425649dbb3dda555bc2ce6e8b69d0
Victorino_01 <central.rrodriguez@gmail.com>
2024-05-26 21:33:09 -0300

    nuevas viwes creadas, nuevos cursos agregados a losodelos con terminal, y nueva funcion, tamplate agregado que retorna la lista de cursos

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from AppCoder.models import Curso
+from django.http import HttpResponse
 
 # Create your views here.
+def curso(req, nombre, camada):
+    nuevo_curso = Curso(nombre=nombre,camada=camada)
+    nuevo_curso.save()
+    return HttpResponse(f"El curso {nuevo_curso.nombre} fue creado con el numero {nuevo_curso.camada}")
+
+def lista_cursos(req):
+    lista = Curso.objects.all()
+    return render(req, "lista_cursos.html", {"lista_cursos": lista}) 
--- ProyectoCoder/urls.py
+++ ProyectoCoder/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from AppCoder.views import curso, lista_cursos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('nuevo-curso/<nombre>/<camada>', curso),
+    path('lista-cursos', lista_cursos),
 ]
